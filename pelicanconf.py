3750b69dce44e776c34754405d633442e69370db
JuliaTayaye <100946580+JuliaTayaye@users.noreply.github.com>
2022-09-14 20:18:11 +0200

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Julia Tayaye Iley'
+SITENAME = "MON BLOG EN LIGNE SUR L'INFLUENCE DE PYTHON"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Univ-Angers', 'https://www.univ-angers.fr/fr/index.html'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Pelican', 'https://getpelican.com/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+SOCIAL = (('Here is the link to my professor linkedin profil :) ', 'https://www.linkedin.com/in/cgailloty/'),
+          ('Here is his github profil', 'https://github.com/agailloty'),
+          ('My linkedin profil', 'https://www.linkedin.com/in/julia-tayaye-iley/'),
+          ('the github profil that he made me create ^-^','https://github.com/JuliaTayaye'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
